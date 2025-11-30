9217a26d8c577fee8856ee3e9822cde7139798b7
unknown <jamepvpz@gmail.com>
2025-11-30 14:31:48 +0700

    day10

--- data.js
+++ data.js
@@ -1,47 +1,55 @@
 const config = {
-    meandyou: "Jame & Minnie",
-    anniversaryDate: new Date(2025, 10, 16),
-    themeColor: "#ff9a9e",
-    musicList: [
-        { title: "Day 1", url: "/misc/music/คลั่งเธอ.mp3" },
-        { title: "Day 2", url: "/misc/music/รถเต่าคันเก่าหลังบ้าน.mp3" },
-        { title: "Day 3", url: "/misc/music/flashlight.mp3" },
-        { title: "Day 4", url: "/misc/music/นับหนึ่ง.mp3" },
-        { title: "Day 5", url: "/misc/music/daisies.mp3" },
-        { title: "Day 6", url: "/misc/music/peaches.mp3" },
-        { title: "Day 7", url: "/misc/music/อีกแล้ว.mp3" },
-        { title: "Day 8", url: "/misc/music/surething.mp3" },
-        { title: "Day 9", url: "/misc/music/คนที่โชคดีกว่า.mp3" },
-    ],
-    musicVolume: 0.1
+  meandyou: "Jame & Minnie",
+  anniversaryDate: new Date(2025, 10, 16),
+  themeColor: "#ff9a9e",
+  musicList: [
+    { title: "Day 1", url: "/misc/music/คลั่งเธอ.mp3" },
+    { title: "Day 2", url: "/misc/music/รถเต่าคันเก่าหลังบ้าน.mp3" },
+    { title: "Day 3", url: "/misc/music/flashlight.mp3" },
+    { title: "Day 4", url: "/misc/music/นับหนึ่ง.mp3" },
+    { title: "Day 5", url: "/misc/music/daisies.mp3" },
+    { title: "Day 6", url: "/misc/music/peaches.mp3" },
+    { title: "Day 7", url: "/misc/music/อีกแล้ว.mp3" },
+    { title: "Day 8", url: "/misc/music/surething.mp3" },
+    { title: "Day 9", url: "/misc/music/คนที่โชคดีกว่า.mp3" },
+    { title: "Day 10", url: "/misc/music/รู้งี้เป็นแฟนตั้งนานแล้ว.mp3" },
+  ],
+  musicVolume: 0.1,
 };
 
 const timelineEvents = [
-    {
-        date: "16-11-2025",
-        title: "วันแรกที่เจอกัน",
-        description: "จำได้ไหมครั้งแรกที่เจอ เค้าเข้าหาบี๋เพราะความเก่งของบี๋"
-    },
-    {
-        date: "19-11-2025",
-        title: "วันที่เค้าเริ่มจีบบี๋จริงจัง",
-        description: "เค้าดีใจและขอบคุณตัวเองที่ตัดสินใจจีบบี๋ในวันนั้น จนเค้ามีบี๋ทุกวันนี้"
-    },
-    {
-        date: "26-11-2025",
-        title: "ครบ 10 วันที่เจอกัน",
-        description: "เค้ามีความสุขมากๆรู้สึกเหมือนได้รับพลังบวกๆทุกๆวัน"
-    }
+  {
+    date: "16-11-2025",
+    title: "วันแรกที่เจอกัน",
+    description: "จำได้ไหมครั้งแรกที่เจอ เค้าเข้าหาบี๋เพราะความเก่งของบี๋",
+  },
+  {
+    date: "19-11-2025",
+    title: "วันที่เค้าเริ่มจีบบี๋จริงจัง",
+    description:
+      "เค้าดีใจและขอบคุณตัวเองที่ตัดสินใจจีบบี๋ในวันนั้น จนเค้ามีบี๋ทุกวันนี้",
+  },
+  {
+    date: "26-11-2025",
+    title: "ครบ 10 วันที่เจอกัน",
+    description: "เค้ามีความสุขมากๆรู้สึกเหมือนได้รับพลังบวกๆทุกๆวัน",
+  },
+  {
+    date: "29-11-2025",
+    title: "ตัดสินใจ",
+    description: "ตัดสินใจจองตั๋วไปหาบี๋โดยไม่ลังเลเลยทั้งๆที่เราพึ่งเจอกัน",
+  },
 ];
 
 const galleryImages = [
-    "misc/img/mylove1.png",
-    "misc/img/mylove2.png",
-    "misc/img/mylove3.png",
-]
+  "misc/img/mylove1.png",
+  "misc/img/mylove2.png",
+  "misc/img/mylove3.png",
+];
 
 const messages = [
-    "ขอบคุณที่น่ารักกับเค้าตลอด ไม่ว่าจะเรื่องอะไรบี๋ยอมเค้าหมดเลย",
-    "บี๋เป็นส่วนสำคัญมากๆที่ทำให้เค้าสดใสสุดๆ มีบี๋เข้ามาแล้วชีวิตแฮปปี้มาก",
-    "ขอบคุณที่เข้าใจกัน ถนอมใจกัน ไม่ปล่อยให้เค้ารู้สึกแย่เลยแม้แต่ครั้งเดียว รักนะ",
+  "ขอบคุณที่น่ารักกับเค้าตลอด ไม่ว่าจะเรื่องอะไรบี๋ยอมเค้าหมดเลย",
+  "บี๋เป็นส่วนสำคัญมากๆที่ทำให้เค้าสดใสสุดๆ มีบี๋เข้ามาแล้วชีวิตแฮปปี้มาก",
+  "ขอบคุณที่เข้าใจกัน ถนอมใจกัน ไม่ปล่อยให้เค้ารู้สึกแย่เลยแม้แต่ครั้งเดียว รักนะ",
+  "อยู่กับบี๋มีแต่ดีขึ้นทุกๆวัน ใจเค้าฟูตลอดที่ได้คุยได้อยู่ด้วย",
 ];
